4f75d964cac8cb978416573389da04ad304ab2d8
yuna-Song-yuna <68729868+yuna-Song-yuna@users.noreply.github.com>
2020-11-11 23:00:20 +0900

    index, db connect
    design is hardest work for me.....

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express();
 var ejs = require("ejs")
+var mysql = require("mysql2")
 
 app.set("view engine", "ejs");
 app.engine("ejs", ejs.renderFile);
-app.use(express.static('/public'))
+app.use(express.static(__dirname+'/public'))    //__dirname 빼먹어서 기본경로가 설정 안됐어 ==> css파일 경로도 못찾았던 원인
 
 var router1 = require('./router1')(app)
 
--- router1.js
+++ router1.js
@@ -1,3 +1,12 @@
+const mysql = require("mysql2")
+let conn_info = {
+    host : 'localhost',
+    port : 3320,
+    user : 'root',
+    password : '1234',
+    database : 'mydb'
+}
+
 module.exports = function(app){
     app.get('/',function(req, res){
         res.render('index.ejs')

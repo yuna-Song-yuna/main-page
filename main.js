9b45abaeea22743e9f92cb1d1a1fe1ee73dbcef2
yuna-Song-yuna <68729868+yuna-Song-yuna@users.noreply.github.com>
2020-11-23 00:14:52 +0900

    passport-naver
    네이버로 로그인 완료, db연동-저장 완료
    
    이제 세션저장하고 세션 여부에 따라 login-logout 바꾸는거 해야함
    
    //카카오톡 로그인은 아직 ㅠㅠ

--- main.js
+++ main.js
@@ -3,17 +3,35 @@ var app = express();
 var ejs = require("ejs")
 var mysql = require("mysql2")
 var passport = require("passport");
+var session = require("express-session");
 var kakaostrategy = require("passport-kakao").Strategy;
+var naverstrategy = require("passport-naver").Strategy;
 
 app.set("view engine", "ejs");
 app.engine("ejs", ejs.renderFile);
 app.use(express.static(__dirname+'/public'))    //__dirname 빼먹어서 기본경로가 설정 안됐어 ==> css파일 경로도 못찾았던 원인
+app.use(passport.initialize());
+app.use(passport.session());
 
+let conn_info = {
+    host : 'localhost',
+    port : 3320,
+    user : 'root',
+    password : '1234',
+    database : 'product'
+}
 
 const kakaoKey = {
     clientID: "b2b62640b451c5d298b88e5b2325b09b",
-    clientsecret: "YmB07uLz8NwbwWIxleOTAgs5pvMaaJ94",
+    clientSecret: "CySxx2jAdMHhnJAS1pzIL8MGiKlEOaGz",
     callbackURL: "http://localhost:2000/login/kakao/callback"
+}
+
+const naverKey = {
+    clientID: "k5XswujMd_tN3fwskIA2",
+    clientSecret: "oRgOjh_XCw",
+    callbackURL: "http://localhost:2000/login/naver/callback"
+
 }
 
 passport.use(new kakaostrategy(kakaoKey, (accessToken, refreshToken, profile, done)=>{
@@ -22,6 +40,34 @@ passport.use(new kakaostrategy(kakaoKey, (accessToken, refreshToken, profile, do
     console.log(refreshToken);
 }))
 
+passport.use(new naverstrategy(naverKey, (accessToken, refreshToken, profile, done)=>{
+    process.nextTick(function(){
+        var user = {
+            name: profile.name,
+            email: profile.emails[0].value,
+            username: profile.displayName,
+            provider: 'naver',
+            naver: profile._json
+        }
+        console.log("user:",user)
+        return done(null, user)
+    })
+    let conn = mysql.createConnection(conn_info)
+    conn.query('select id from guest_naver where id=?',profile._json.id,(err, result)=>{
+        if(result.length == 0){
+            let sql = 'insert into guest_naver values (?,?,?,?,?)'
+            let val = [profile._json.id,profile.emails[0].value,profile._json.age,profile._json.name,profile._json.birthday]
+            conn.query(sql, val, (err)=>{
+                console.log(err)
+            })                
+        }
+    })
+}))
+
+//naver api 쓰는데 failed to serialize user into session 에러 발생해서 추가했음
+passport.serializeUser(function(user, done){
+    done(null, user)
+})
 
 var router1 = require('./router1')(app, passport)
 
--- router1.js
+++ router1.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2")
+const request = require("request");
 let conn_info = {
     host : 'localhost',
     port : 3320,
     user : 'root',
     password : '1234',
-    database : 'mydb'
+    database : 'product'
 }
 
 module.exports = function(app, passport){
@@ -21,6 +22,27 @@ module.exports = function(app, passport){
     app.get('/login/kakao/callback', passport.authenticate('kakao', {
         successRedirect: '/',
         failureRedirect: '/login'
-    })
+        })
     )
+
+    // app.get('/login/kakao/callback',function(req, res, next){
+    //     passport.authenticate('kakao', function(err, user){
+    //         console.log('passport.authenticate(kakao)실행')
+    //         if (!user){
+    //             return res.redirect('http://locathost:2000/login')
+    //         }
+    //         req.login(user, function(err){
+    //             console.log('kakao/callback user:', user);
+    //             return req.redirect('http://localhost:2000/');
+    //         });
+    //     });(req,res)
+    // })
+
+    app.get('/login/naver', passport.authenticate('naver'))
+
+    app.get('/login/naver/callback', passport.authenticate('naver', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
 }

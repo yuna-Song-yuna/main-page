1bebb545d7c35ceb0045e35e2fc7ef1a2402537c
yuna-Song-yuna <68729868+yuna-Song-yuna@users.noreply.github.com>
2020-12-25 23:11:37 +0900

    판매등록 기능 추가
    판매등록 시 데이터 저장 + 메인페이지에 뿌려주기 기능 추가

--- router1.js
+++ router1.js
@@ -22,9 +22,14 @@ let conn_info = {
 
 module.exports = function(app, passport){
     app.get('/',function(req, res){
-        console.log('passport.user:',req.session.passport)
-        console.log('session:',req.session)
-        res.render('index.ejs', {session:req.session.passport})
+        // console.log('passport.user:',req.session.passport)
+        // console.log('session:',req.session)
+        let conn=mysql.createConnection(conn_info);
+        let sql = 'select * from sell_product'
+        conn.query(sql, (err, result)=>{
+            console.log('result:',result)
+            res.render('index.ejs', {session:req.session.passport, result})
+        })
     })
     
     app.get('/login', function(req, res){
@@ -74,6 +79,7 @@ module.exports = function(app, passport){
         res.render('regi_item.ejs',{session:req.session.passport})
     })
 
+    //판매 등록
     app.post('/regi_item_ing',parser, function(req, res){
         if(!req.files){     //이미지를 하나라도 전달받으면 등록안한 이미지는 undefined로 전송이 됨 => 에러 안나고 진행
                             //근데 이미지를 둘다 전송 안하면 null로 전송됨 => 에러남
@@ -88,7 +94,7 @@ module.exports = function(app, passport){
         // console.log('req.files.content_image:',req.files.content_image)
 
         let conn = mysql.createConnection(conn_info);
-        if(!req.files){
+        if(!req.files){ //이미지 둘 다 등록 안한 경우
             let sql = 'insert into sell_product (id, title, price, category, start_date, content_text) values (?,?,?,?,?,?)'
             input_data = [req.session.passport.user, req.body.title, req.body.price, req.body.category, req.body.start_date, req.body.content_text]
             conn.query(sql, input_data, (err, result)=>{
@@ -97,16 +103,34 @@ module.exports = function(app, passport){
             console.log('아이디:',req.session.passport.user)
             console.log(req.body.category)      //name='category' <option>의 value값이 들어옴
             console.log(req.body.start_date)
-        }else if(!req.files.sell_image){
+        }else if(!req.files.sell_image){    //썸네일 이미지 등록 안한 경우
+            content_image.mv(__dirname+'/public/image/uploads/'+content_image.name) //서버에 이미지 저장
+
+            let sql = 'insert into sell_product (id, title, price, category, start_date, content_text, content_image) values (?,?,?,?,?,?,?)'
+            input_data = [req.session.passport.user, req.body.title, req.body.price, req.body.category, req.body.start_date, req.body.content_text, content_image.name]
+            conn.query(sql, input_data, (err, result)=>{
+                console.log(err)
+                res.redirect('/')
+            })
+        }else if(!req.files.content_image){ //상세내용 이미지 등록 안한 경우
+            sell_image.mv(__dirname+'/public/image/uploads/'+sell_image.name)
+
+            let sql = 'insert into sell_product (id, title, price, category, start_date, content_text, thumbnail_image) values (?,?,?,?,?,?,?)'
+            input_data = [req.session.passport.user, req.body.title, req.body.price, req.body.category, req.body.start_date, req.body.content_text, sell_image.name]
+            conn.query(sql, input_data, (err, result)=>{
+                console.log(err)
+                res.redirect('/')
+            })
+        }else{  //이미지 모두 등록 한경우
+            sell_image.mv(__dirname+'/public/image/uploads/'+sell_image.name)
             content_image.mv(__dirname+'/public/image/uploads/'+content_image.name)
-            res.redirect('/')
-        }else if(!req.files.content_image){
-            sell_image.mv(__dirname+'/public/image/uploads/'+sell_image.name)
-            res.redirect('/')
-        }else{
-            sell_image.mv(__dirname+'/public/image/uploads/'+sell_image.name)
-            content_image.mv(__dirname+'/public/image/uploads/'+content_image.name)  
-            res.redirect('/')  
+
+            let sql = 'insert into sell_product (id, title, price, category, start_date, content_text, content_image, thumbnail_image) values (?,?,?,?,?,?,?,?)'
+            input_data = [req.session.passport.user, req.body.title, req.body.price, req.body.category, req.body.start_date, req.body.content_text, content_image.name, sell_image.name]
+            conn.query(sql, input_data, (err, result)=>{
+                console.log(err)
+                res.redirect('/')
+            })
         }
         
         console.log(req.body)
